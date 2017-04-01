27795c4f27bc8e1994f96f5ef9fc252350b9228e
DivineOmega <jordan@hall05.co.uk>
2017-04-01 19:40:41 +0100

    Ability to create or update a Response object from a Laravel Request object

--- src/Models/Response.php
+++ src/Models/Response.php
@@ -2,8 +2,12 @@
 
 namespace DivineOmega\LaravelMultipleChoice\Models;
 
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use DivineOmega\LaravelMultipleChoice\Models\Question;
+use DivineOmega\LaravelMultipleChoice\Models\Choice;
+use DivineOmega\LaravelMultipleChoice\Models\ResponseItem;
 
 class Response extends Model
 {
@@ -14,5 +18,48 @@ class Response extends Model
     public function items()
     {
         return $this->hasMany('DivineOmega\LaravelMultipleChoice\Models\ResponseItem');
+    }
+
+    public static function createFromRequest(Request $request)
+    {
+        $response = new self;
+        $response->save();
+
+        return $this->updateFromRequest($request, $response->id);
+    }
+
+    public static function updateFromRequest(Request $request, $responseId)
+    {
+        $response = self::FindOrFail($responseId);
+
+        $inputs = $request->all();
+
+        foreach($inputs as $name => $choiceId) {
+
+            if (strpos($name, 'lmc-question-input-')===false) {
+                continue;
+            }
+
+            $questionId = str_replace('lmc-question-input-', '', $name);
+            if (!is_numeric($questionId)) {
+                continue;
+            }
+
+            $question = Question::FindOrFail($questionId);
+            
+            if (!is_numeric($choiceId)) {
+                continue;
+            }
+
+            $choice = Choice::FindOrFail($choiceId);
+
+            $responseItem = new Response;
+            $responseItem->response_id = $response->id;
+            $responseItem->question_id = $question->id;
+            $responseItem->choice_id = $choice->id;
+            $responseItem->save();
+        }
+
+        return $response;
     }
 }
