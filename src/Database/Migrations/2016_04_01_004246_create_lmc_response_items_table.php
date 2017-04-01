8f83768a4e5a6a71a1915814aaec21c6d2ad9b8b
DivineOmega <jordan@hall05.co.uk>
2017-04-01 16:46:43 +0100

    Correct migration

--- src/Database/Migrations/2016_04_01_004246_create_lmc_response_items_table.php
+++ src/Database/Migrations/2016_04_01_004246_create_lmc_response_items_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLmcResponseTable extends Migration
+class CreateLmcResponseItemsTable extends Migration
 {
     /**
      * Run the migrations.
