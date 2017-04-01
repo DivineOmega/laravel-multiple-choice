cfe9755f627c0dbc486a5eccc6df826e5bbc18f0
DivineOmega <jordan@hall05.co.uk>
2017-04-01 01:28:55 +0100

    Initial QuestionManager object

---
+++ src/Utils/QuestionManager.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace DivineOmega\LaravelMultipleChoice\Utils;
+
+use DivineOmega\LaravelMultipleChoice\Models\Question;
+use DivineOmega\LaravelMultipleChoice\Models\Choice;
+
+class QuestionManager
+{
+    public function put($questionIdOrText, array $choicesText)
+    {
+        $question = null;
+
+        // Attempt to find question
+        if (is_numeric($questionIdOrText)) {
+            $question = Question::FindOrFail($questionIdOrText);
+        } else {
+            $question = Question::where('text', $questionIdOrText)->first();
+        }
+
+        // Create question if it does not exist
+        if (!$question) {
+            $question = new Question;
+            $question->text = $questionIdOrText;
+            $question->save();
+        }
+
+
+        // Delete choices with text not in choicesText array
+        $choicesToDelete = $question->choices()->whereNotIn('text', $choicesTexts)->get();
+        foreach($choicesToDelete as $choiceToDelete) {
+            $choiceToDelete->delete();
+        }
+
+        // Create any choices from choicesText array that do not already exist
+        foreach($choicesTexts as $choicesText) {
+
+            $choice = $question->choices()->where('text', $choicesText)->first();
+
+            if ($choice) {
+                continue;
+            }
+
+            $choice = new Choice;
+            $choice->question_id = $question->id;
+            $choice->text = $choicesText;
+            $choice->save();
+        }
+    }
+
+}
