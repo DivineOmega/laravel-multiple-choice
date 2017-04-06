582b93a24cf1af0285001b90973f1d1ca6369964
Jordan Hall <jordan.hall@rapidweb.biz>
2017-04-06 09:12:20 +0100

    Add description to lmc_choices table

---
+++ src/Database/Migrations/2016_04_01_003940_add_description_to_lmc_choices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLmcChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lmc_choices', function (Blueprint $table) {
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lmc_choices', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
