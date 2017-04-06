474be0b599bb1c0fc7290e4ec7418c4e8daf12c3
DivineOmega <jordan@hall05.co.uk>
2017-04-06 23:20:37 +0100

    Fix class name

--- src/Database/Migrations/2016_04_01_003940_add_description_to_lmc_choices_table.php
+++ src/Database/Migrations/2016_04_01_003940_add_description_to_lmc_choices_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLmcChoicesTable extends Migration
+class AddDescriptionToLmcChoicesTable extends Migration
 {
     /**
      * Run the migrations.
